ef94384ccd88eb6a9bf0642842762cd69192af1c
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-24 12:28:11 +0530

    Update DetectCycleInUnDirectedGraph.cpp

--- DetectCycleInUnDirectedGraph.cpp
+++ DetectCycleInUnDirectedGraph.cpp
@@ -28,10 +28,49 @@ bool bfs(int V, int source, vector<vector<int>> &adj, vector<bool> vis){
     return 0;
 }
 
+bool dfsWithStack(int V, int S, vector<int> adj[], vector<bool> vis){
+    stack<pair<int, int>> sta;
+    sta.push({S, -1});
+    vis[S] = 1;
+    while(!sta.empty()){
+        int child = sta.top().first;
+        int parent = sta.top().second;
+        sta.pop();
+        for(int &it: adj[child]){
+            if(parent!=it){
+                if(!vis[it]){
+                    sta.push({it, child});
+                    vis[it] = 1;
+                }
+                else    return 1;
+            }
+        }
+    }
+    return 0;
+}
+bool dfs(int source, int parent, vector<int> adj[], vector<bool> &vis){
+    vis[source] = 1;
+    for(int &it: adj[source]){
+        if(it!=parent){
+            if(vis[it])     return 1;
+            else            dfs(it, source, adj, vis);
+        }
+    }
+    return 0;
+}
+
 bool isCycle(int V, vector<vector<int>> &adj) {
     vector<bool> vis(V, 0);
     for(int i=0; i<V; i++){
-        if(bfs(V, i, adj, vis))      return 1;
+        // bfs
+        // if(bfs(i, adj, vis))         return 1;
+            
+        // dfs with stack
+        // if(dfsWithStack(i, adj, vis))         return 1;
+            
+        // dfs recursion
+        // vector<bool> vis(V, 0);
+        // if(dfs(i, -1, adj, vis))            return 1;
     }
     return 0;
 }
