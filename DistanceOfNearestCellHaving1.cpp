c5ffbe579e9ed953ed1c8185de496df3b2ea256d
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-25 13:01:38 +0530

    Update DistanceOfNearestCellHaving1.cpp

--- DistanceOfNearestCellHaving1.cpp
+++ DistanceOfNearestCellHaving1.cpp
@@ -22,16 +22,16 @@ vector<vector<int>>nearest(vector<vector<int>>&grid)
 	vector<vector<bool>> vis(n, vector<bool>(m , 0));
 	vector<vector<int>> res(n, vector<int>(m , 0));
 	queue<S> q1;
-    for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (grid[i][j] == 1) { 
-                q1.push({i, j, 0});
-                vis[i][j] = 1;
-                res[i][j] = 0;
+			if (grid[i][j] == 1) {
+				q1.push({i, j, 0});
+				vis[i][j] = 1;
+				res[i][j] = 0;
 			}
 		}
 	}
-    
+
 	while (!q1.empty()) {
 		int R = q1.front().x;
 		int C = q1.front().y;
@@ -40,8 +40,10 @@ vector<vector<int>>nearest(vector<vector<int>>&grid)
 		for (int i = 0; i < 4; i++) {
 			int nRow = R + dx[i];
 			int mCol = C + dy[i];
-			if (nRow >= 0 && mCol >= 0 && nRow < n && mCol < m && grid[nRow][mCol] == 0 && !vis[nRow][mCol]) {
-                        	q1.push({nRow, mCol, V});
+			if (nRow >= 0 && mCol >= 0 &&
+			        nRow < n && mCol < m &&
+			        grid[nRow][mCol] == 0 && !vis[nRow][mCol]) {
+				q1.push({nRow, mCol, V});
 				vis[nRow][mCol] = 1;
 				res[nRow][mCol] = V;
 			}
@@ -51,26 +53,26 @@ vector<vector<int>>nearest(vector<vector<int>>&grid)
 }
 
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    int n,m;        cin>>n>>m;
-    vector<vector<int>> grid;
-    for(int i=0; i<n; i++){
-        vector<int> a;
-        for(int j=0; j<m; j++){
-            int c;  cin>>c;
-            a.pb(c);
-        }
-        grid.pb(a);
-    }
-    cout<<nl;
-    vector<vector<int>> ans = nearest(grid);
-    for(auto &it: ans){
-        for(auto &i: it)  cout<<i<<" ";
-        cout<<nl;
-    }
+	int n, m;        cin >> n >> m;
+	vector<vector<int>> grid;
+	for (int i = 0; i < n; i++) {
+		vector<int> a;
+		for (int j = 0; j < m; j++) {
+			int c;  cin >> c;
+			a.pb(c);
+		}
+		grid.pb(a);
+	}
+	cout << nl;
+	vector<vector<int>> ans = nearest(grid);
+	for (auto &it : ans) {
+		for (auto &i : it)  cout << i << " ";
+		cout << nl;
+	}
 
-    return 0;
+	return 0;
 }
