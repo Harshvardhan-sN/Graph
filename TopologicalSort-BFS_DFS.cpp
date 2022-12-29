ed24af5f00a90bd4be44112cf7a6fda64cb336f7
Harshvardhan Singh Negi <81630801+Harshvardhan-sN@users.noreply.github.com>
2022-12-29 21:22:08 +0530

    Add files via upload

--- TopologicalSort-BFS_DFS.cpp
+++ TopologicalSort-BFS_DFS.cpp
@@ -9,7 +9,7 @@ using namespace std;
 #define FOR(i, j, n) for (int i = j; i < n; i++)
 #define pb push_back
 
-void dfs(int S, vector<vector<int>> &adj, vector<bool> &vis, stack<int> &st) {
+void dfs(int S, vector<int> adj[], vector<bool> &vis, stack<int> &st) {
 	vis[S] = 1;
 	for (int &child : adj[S]) {
 		if (!vis[child]) {
@@ -19,7 +19,7 @@ void dfs(int S, vector<vector<int>> &adj, vector<bool> &vis, stack<int> &st) {
 	st.push(S);
 }
 
-vector<int> topoSort(int V, vector<vector<int>> &adj) {
+void topoSortByDFS(int V, vector<int> adj[]) {
 	vector<bool> vis(V, 0);
 	stack<int> st;
 	for (int i = 0; i < V; i++) {
@@ -29,69 +29,86 @@ vector<int> topoSort(int V, vector<vector<int>> &adj) {
 	}
 	vector<int> res;
 	while (!st.empty()) {
-		res.push_back(st.top());
+		cout << st.top() << " ";
 		st.pop();
 	}
-	return res;
+	cout << endl;
+}
+
+void topoSortByBFS(int V, vector<int> adj[]) {
+	vector<int> InDegree(V, 0); 		// map<int, int> InDegree
+	queue<int> q1;
+	for (int i = 0; i < V; i++) {
+		for (int &it : adj[i])    InDegree[it]++;
+	}
+
+	for (int i = 0; i < V; i++) {
+		if (InDegree[i] == 0)    q1.push(i);
+	}
+	vector<int> res;
+	while (!q1.empty()) {
+		int S = q1.front();
+		q1.pop();
+		res.push_back(S);
+
+		for (int &child : adj[S]) {
+			InDegree[child]--;
+			if (InDegree[child] == 0)    q1.push(child);
+		}
+	}
+	if (res.size() == V) {
+		for (auto &it : res)      cout << it << " ";
+		cout << endl;
+	}
+	else {
+		cout << "Cycle Detected No TopoSort\n";
+	}
 }
 
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);	cout.tie(NULL);
-	
+
 	vector<vector<int>> dag;
 
-    // // testCase -> 1
-    // int V = 6;
-	// dag.push_back({1});
-	// dag.push_back({2});
-	// dag.push_back({3});
-	// dag.push_back({4, 5});
-	// dag.push_back({});
-	// dag.push_back({});
+	// // testcase -> 1
+	int V = 6;
+	dag.push_back({5, 2});
+	dag.push_back({5, 0});
+	dag.push_back({4, 0});
+	dag.push_back({4, 1});
+	dag.push_back({2, 3});
+	dag.push_back({3, 1});
 
-    // // testCase -> 2
-    int V = 6;
-    dag.push_back({});
-    dag.push_back({});
-    dag.push_back({3});
-    dag.push_back({1});
-    dag.push_back({0, 1});
-    dag.push_back({0, 2});
+	// // testCase -> 2
+	// int V = 4;
+	// dag.push_back({0, 1});
+	// dag.push_back({1, 2});
+	// dag.push_back({2, 3});
+	// dag.push_back({3, 3});
 
-
-    // ************  DFS  ****************
-	vector<int> order = topoSort(V, dag);
-	for (auto &it : order)    cout << it << " ";
-    cout<<nl;
+	// // testCase -> 3
+	// int V = 4;
+	// dag.push_back({3, 0});
+	// dag.push_back({1, 0});
+	// dag.push_back({2, 0});
 
 
-
-    // ************  BFS  ****************
-    vector<int> InDegree(V, 0); 		// map<int, int> InDegree
-	queue<int> q1;
-    for(int i=0; i<V; i++){
-	    for(int &it: dag[i])    InDegree[it]++;
-	}    
-	for(int i=0; i<V; i++){
-	    if(InDegree[i]==0)    q1.push(i);
+	// Create Adjacency List of given Graph
+	vector<int> adj[V];
+	for (auto &it : dag) {
+		adj[it[0]].push_back(it[1]);
 	}
-    vector<int> res;
-	while(!q1.empty()){
-        int S = q1.front();
-	    q1.pop();
-	    res.push_back(S);
-	        
-	    for(int &child: dag[S]){
-	        InDegree[child]--;
-	        if(InDegree[child]==0)    q1.push(child);
-	    }
-	}
-    for(auto &it: res)      cout<<it<<" ";
+
+	// ************  DFS  ****************
+	topoSortByDFS(V, adj);
+
+	// ************  BFS  ****************
+	topoSortByBFS(V, adj);
 	/*
-	to detect a cycle in directed graph using topological sort 
-	just check if the topological sort for that graph exsit or not 
+	to detect a cycle in directed graph using topological sort
+	just check if the topological sort for that graph exsit or not
 	if it exist then it donot have cycle otherwise yes
 	*/
 
